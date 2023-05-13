f4f460aafcfb0a28256b6adc62746576a25833e6
Brian Kesner <brk828@gmail.com>
2023-05-13 19:13:36 +1000

    Renamed estimates script and added Reach estimates.

--- PopulationEstimates.R
+++ PopulationEstimates.R
@@ -65,6 +65,29 @@ BasinRecapturesZoneV <- BasinMarksZoneV %>%
                     "CensusYear" = "CensusYear", 
                     "PITIndex" = "PITIndex"))
 
+# Reach specific estimates require any contact that matches by Reach
+BasinMarksReachV <- BasinMarksZoneV %>%
+  group_by(Reach, CensusYear, Species, PITIndex) %>%
+  summarise(ContactReaches = n(), 
+            MinZone = min(DecimalZone), 
+            MaxZone = max(DecimalZone)) %>%
+  ungroup()
+
+BasinCapturesReachV <- BasinCapturesZoneV %>%
+  group_by(Reach, CensusYear, Species, PITIndex) %>%
+  summarise(ContactReaches = n(), 
+            MinZone = min(DecimalZone), 
+            MaxZone = max(DecimalZone)) %>%
+  ungroup()
+  
+
+BasinRecapturesReachV <- BasinMarksReachV %>%
+  inner_join(BasinCapturesReachV %>%
+               select(Reach, CensusYear, PITIndex), 
+             by = c("Reach" = "Reach",
+                    "CensusYear" = "CensusYear", 
+                    "PITIndex" = "PITIndex"))
+
 rm(BasinContacts, BasinCapturesZone, BasinMarksZone)
 
 ZoneMark <- BasinMarksZoneV %>%
@@ -82,6 +105,21 @@ ZoneRecapture <- BasinRecapturesZoneV %>%
   summarise(R = n_distinct(PITIndex)) %>%
   ungroup()
 
+ReachMark <- BasinMarksReachV %>%
+  group_by(Species, Reach, CensusYear) %>%
+  summarise(M = n_distinct(PITIndex)) %>%
+  ungroup()
+
+ReachCapture <- BasinCapturesReachV %>%
+  group_by(Species, Reach, CensusYear) %>%
+  summarise(C = n_distinct(PITIndex)) %>%
+  ungroup()
+
+ReachRecapture <- BasinRecapturesReachV %>%
+  group_by(Species, Reach, CensusYear) %>%
+  summarise(R = n_distinct(PITIndex)) %>%
+  ungroup()
+
 rm(BasinCapturesZoneV, BasinMarksZoneV, BasinRecapturesZoneV)  
 
 ZoneEstimates <- ZoneMark %>%
@@ -93,6 +131,21 @@ ZoneEstimates <- ZoneMark %>%
                                  "CensusYear" = "CensusYear")) %>%
   filter(R>3) %>%
   arrange(Species, DecimalZone, CensusYear) %>%
+  mutate(LowerBoundR = qpois(0.05, R),
+         UpperBoundR = qpois(0.95, R),
+         Estimate = ((M + 1) * (C + 1))/(R + 1)) %>%
+  mutate(Lower95CI = ((M + 1) * (C + 1))/(UpperBoundR + 1),
+         Upper95CI = ((M + 1) * (C + 1))/(LowerBoundR + 1))
+
+ReachEstimates <- ReachMark %>%
+  inner_join(ReachCapture, by = c("Species" = "Species",
+                                 "Reach" = "Reach",
+                                 "CensusYear" = "CensusYear")) %>%
+  inner_join(ReachRecapture, by = c("Species" = "Species",
+                                   "Reach" = "Reach",
+                                   "CensusYear" = "CensusYear")) %>%
+  filter(R>3) %>%
+  arrange(Species, Reach, CensusYear) %>%
   mutate(LowerBoundR = qpois(0.05, R),
          UpperBoundR = qpois(0.95, R),
          Estimate = ((M + 1) * (C + 1))/(R + 1)) %>%
