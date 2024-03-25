dc756b552e3c5ce702fd4059c24537d253d5ff28
Brian Kesner <brk828@gmail.com>
2024-03-25 12:36:15 -0700

    Trip Reporting update

--- TripReporting.R
+++ TripReporting.R
@@ -7,7 +7,7 @@ source("LabFunctions.R")
 packages(dplyr)
 packages(lubridate)
 packages(zoo)
-
+packages(openxlsx) # package openxlsx is required
 # remove unnecessary functions
 rm(euclid, split_hourly, download_nfwg, download_backwater)
 
@@ -32,30 +32,47 @@ if(file.exists("data/BasinScanningIndex.RData")){
 rm(download_basin, data_info, data_date, Unit, ReachTable)
 
 ReachTrips <- TripTable %>% 
-  filter(TripReach == StudyReach, TripStart > (Sys.Date() - months(3))) %>%
+  filter(TripReach == StudyReach, 
+         Isolated == "No", 
+         TripStart > (Sys.Date() - months(6))) %>%
   arrange(desc(TripStart)) %>% select(-TripReach, Isolated)
 rm(TripTable)
 
 TripContacts <- BasinContacts %>%
-  inner_join(ReachTrips %>% select(TID), by = c("TripID" = "TID")) %>%
+  inner_join(ReachTrips %>% select(TID, TripStart, TripEnd), 
+             by = c("TripID" = "TID")) %>%
   left_join(BasinPITIndex %>% 
               select(PIT, PITIndex, Species, Sex, DateVerified,
                      ReleaseDate, ReleaseLocation), 
             by = c("PIT" = "PIT")) %>%
-  group_by(Species, TripID, Location, PITIndex, PIT, DateVerified) %>%
+  group_by(Species, TripID, TripStart, TripEnd, Location, PITIndex, PIT, DateVerified, 
+           ReleaseDate, ReleaseLocation) %>%
   summarise(Contacts=n()) %>%
   ungroup()
 
 TripLocationContactSummary <- TripContacts %>%
-  filter(!is.na(PITIndex)) %>%
-  group_by(TripID, Species, Location) %>%
-  summarise(UniqueFish = n_distinct(PITIndex)) %>%
+  group_by(TripID, TripStart, TripEnd, Species, Location) %>%
+  summarise(UniqueFish = n_distinct(PITIndex, na.rm = TRUE)) %>%
   ungroup()
 
 TripSummary <- ReachTrips %>%
   left_join(TripContacts %>%
               group_by(TripID) %>%
               summarise(UniquePIT = n_distinct(PIT), 
-                        UniqueFish = n_distinct(PITIndex)) %>%
+                        UniqueFish = n_distinct(PITIndex, na.rm = TRUE)) %>%
               ungroup(), by = c("TID" = "TripID"))
 
+TripSummary[is.na(TripSummary)] <- 0
+
+wb <- createWorkbook() # creates object to hold workbook sheets
+
+addWorksheet(wb, "TripsSummary") # add worksheet
+addWorksheet(wb, "LocationSummary") # add worksheet
+addWorksheet(wb, "LocationContacts") # add worksheet
+
+writeData(wb, "TripsSummary", TripSummary) # write dataframe
+writeData(wb, "LocationSummary", TripLocationContactSummary) # write dataframe 
+writeData(wb, "LocationContacts", TripContacts) # write dataframe 
+
+saveWorkbook(wb, paste0("output/TripReportingR", StudyReach, "_",
+                        format(Sys.time(), "%Y%m%d%H%M"), ".xlsx"))
