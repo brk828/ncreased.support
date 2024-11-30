ecbf5882aa234e5d220e0df487cac22ae7ce4920
Brian K <brk828@gmail.com>
2024-11-30 12:02:41 -0700

    FY summary not species specific

--- ReachFYCaptureResults.R
+++ ReachFYCaptureResults.R
@@ -4,16 +4,15 @@
 
 # Assign Study Reach
 StudyReach = 3
-StartDate = "2010-01-01"
-EndDate = "2023-12-31"
-Sp = "XYTE"
+StartDate = "2023-01-01"
+EndDate = "2024-04-30"
 source("LabFunctions.R")
 
 packages(dplyr)
 packages(lubridate)
 
 # remove unnecessary functions
-rm(euclid, split_hourly, download_basin, download_backwater)
+rm(euclid, split_hourly, download_backwater)
 
 # LabFunctions has a timout setting, but isn't retained
 options(timeout = 400)
@@ -25,7 +24,7 @@ if(file.exists("data/NFWGTable.RData")){
   if(data_date>Sys.Date() - 7){
     load("data/NFWGTable.RData")
   } else {
-    download_basin("data")
+    download_nfwg("data")
     load("data/NFWGTable.RData")
   }
 } else {
@@ -33,34 +32,52 @@ if(file.exists("data/NFWGTable.RData")){
   load("data/NFWGTable.RData")
 }
 
-rm(download_nfwg)
+if(file.exists("data/BasinScanningIndex.RData")){
+  data_info <- file.info("data/BasinScanningIndex.RData")
+  data_date <- as.Date(data_info$mtime)
+  if(data_date>Sys.Date() - 7){
+    load("data/BasinScanningIndex.RData")
+  } else {
+    download_basin("data")
+    load("data/BasinScanningIndex.RData")
+  }
+} else {
+  download_basin("data")
+  load("data/BasinScanningIndex.RData")
+}
+
+
+rm(download_nfwg, download_basin)
 # Reduce data to Study Reach, checking to ensure there are no orphans
+
+ReachTagging <- NFWGTable %>% filter(Status == "Capture"|Status == "Release", 
+                                      Recapture == "N",
+                                      Reach == StudyReach) %>%
+select(TaggingEvent = Status, TaggingDate = CollectionDate, TaggingZone = DecimalZone, 
+       TaggingLocation = Location, FirstPIT, TaggingMethod = Method, TaggingTL = TL) # %>%
+#   inner_join(BasinPITIndex %>% select(PITIndex), by = c("FirstPIT" = "PITIndex"))
 
 ReachCaptures <- NFWGTable %>% filter(Status == "Capture", 
                                       Reach == StudyReach, 
-                                      Species == Sp,
                                       CollectionDate >= as.Date(StartDate), 
                                       CollectionDate <= as.Date(EndDate)) %>%
-  select(Species, CollectionDate, DecimalZone, Location, Latitude, Longitude,
-         ReservoirKm, FirstPIT, First134PIT, Collector, Method) %>%
-  arrange(CollectionDate)
-
-ReachReleases <- NFWGTable %>% filter(Status == "Release", 
-                                      Reach == StudyReach, 
-                                      Species == Sp) %>%
-  select(Species, CollectionDate, DecimalZone, Location, Latitude, Longitude,
-         ReservoirKm, FirstPIT, First134PIT, Collector, Method)
+  select(Reach, Zone = DecimalZone, PIT = FirstPIT, PIT134 = First134PIT, CollectionLocation = Location, 
+         CollectionDate, Connection = SurfaceConnection, Species, Sex, TL, Method) %>%
+  arrange(CollectionDate, PIT) %>% 
+#  left_join(BasinPITIndex %>% select(PITIndex, DateVerified, ReleaseDate, ReleaseLocation, ReleaseTL, FirstCensus),
+#            by = c("PIT" = "PITIndex")) %>%
+  left_join(ReachTagging, by = c("PIT" = "FirstPIT"))
 
 packages(openxlsx) # package openxlsx is required
 wb <- createWorkbook() # creates object to hold workbook sheets
 addWorksheet(wb, "ReachCaptures") # add worksheet
-addWorksheet(wb, "ReachReleases") # add worksheet
+# addWorksheet(wb, "ReachReleases") # add worksheet
 
 writeData(wb, "ReachCaptures", ReachCaptures) # write dataframe (second argument) to worksheet
-writeData(wb, "ReachReleases", ReachReleases) # write dataframe (second argument) to worksheet
+# writeData(wb, "ReachReleases", ReachReleases) # write dataframe (second argument) to worksheet
 
 # Last step is to save workbook. Give a useful name.  Adding date time ensures this step
 # will not overwrite a previous version.  Location should be output folder
-saveWorkbook(wb, paste0("output/Reach",StudyReach, Sp, "HandlingRecords",
+saveWorkbook(wb, paste0("output/Reach",StudyReach, "HandlingRecords",
                         format(Sys.time(), "%Y%m%d"), ".xlsx"),
              overwrite = TRUE)
